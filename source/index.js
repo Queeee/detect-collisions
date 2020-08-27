1f177bd4637f30980dc2cc50768dbd0ef9b2056c
Queeee <queyiping@cvte.com>
2020-08-27 15:13:02 +0800

    [feat]add cache map

--- source/index.js
+++ source/index.js
@@ -11,57 +11,111 @@ class Collisions {
   constructor () {
     /** @private */
     this._bvh = new BVH()
+
+    /** @private */
+    this._id2Body = new Map()
+  }
+
+  /**
+   * 插入body到树中
+   * 
+   * @param {String} [i = 0] unique id
+   * @param {Object} body 
+   * @param {Boolean} [updating = false] Set to true if the body already exists in the BVH (used internally when updating the body's position)
+   */
+  _insertBody(id, body, updating = false) {
+    this._bvh.insert(body, updating);
+    this._id2Body.set(id, body);
+    console.log("size: " + this._id2Body.size);
   }
 
   /**
    * Creates a {@link Circle} and inserts it into the collision system
+   * @param {String} [i = 0] unique id
    * @param {Number} [x = 0] The starting X coordinate
    * @param {Number} [y = 0] The starting Y coordinate
    * @param {Number} [radius = 0] The radius
+   * @param {Boolean} [updating = false] Set to true if the body already exists in the BVH (used internally when updating the body's position)
    * @param {Number} [scale = 1] The scale
    * @param {Number} [padding = 0] The amount to pad the bounding volume when testing for potential collisions
    * @returns {Circle}
    */
-  createCircle (x = 0, y = 0, radius = 0, scale = 1, padding = 0) {
+  createCircle (id, x = 0, y = 0, radius = 0, updating = false, scale = 1, padding = 0) {
     const body = new Circle(x, y, radius, scale, padding)
-
-    this._bvh.insert(body)
-
+    this._insertBody(id, body)
     return body
   }
 
   /**
    * Creates a {@link Polygon} and inserts it into the collision system
+   * @param {String} [i = 0] unique id
    * @param {Number} [x = 0] The starting X coordinate
    * @param {Number} [y = 0] The starting Y coordinate
    * @param {Array<Number[]>} [points = []] An array of coordinate pairs making up the polygon - [[x1, y1], [x2, y2], ...]
+   * @param {Boolean} [updating = false] Set to true if the body already exists in the BVH (used internally when updating the body's position)
    * @param {Number} [angle = 0] The starting rotation in radians
    * @param {Number} [scale_x = 1] The starting scale along the X axis
    * @param {Number} [scale_y = 1] The starting scale long the Y axis
    * @param {Number} [padding = 0] The amount to pad the bounding volume when testing for potential collisions
    * @returns {Polygon}
    */
-  createPolygon (x = 0, y = 0, points = [[0, 0]], angle = 0, scale_x = 1, scale_y = 1, padding = 0) {
+  createRelativePolygon (id, x = 0, y = 0, points = [[0, 0]], updating = false, angle = 0, scale_x = 1, scale_y = 1, padding = 0) {
     const body = new Polygon(x, y, points, angle, scale_x, scale_y, padding)
-
-    this._bvh.insert(body)
-
+    this._insertBody(id, body)
     return body
   }
 
   /**
+   * Creates a {@link Polygon} and inserts it into the collision system
+   * 
+   * @param {String} [i = 0] unique id
+   * @param {Array<Number[]>} [points = []] An array of coordinate pairs making up the polygon - [[x1, y1], [x2, y2], ...]
+   * @param {Boolean} [updating = false] Set to true if the body already exists in the BVH (used internally when updating the body's position)
+   * @param {Number} [angle = 0] The starting rotation in radians
+   * @param {Number} [scale_x = 1] The starting scale along the X axis
+   * @param {Number} [scale_y = 1] The starting scale long the Y axis
+   * @param {Number} [padding = 0] The amount to pad the bounding volume when testing for potential collisions
+   * @returns {Polygon}
+   */
+  createAbsolutePolygon (id, points = [[0, 0]], updating = false, angle = 0, scale_x = 1, scale_y = 1, padding = 0) {
+    const startPointX = points[0][0];
+    const startPointY = points[0][1];
+    let relativePoints = [];
+    points.forEach(point => {            
+      let _point = [];
+      _point[0] = point[0] - startPointX;
+      _point[1] = point[1] - startPointY;
+      relativePoints.push(_point);
+    });
+    
+    return this.createRelativePolygon(id, startPointX, startPointY, relativePoints, updating, angle, scale_x, scale_y, padding);
+  }
+
+  /**
    * Creates a {@link Point} and inserts it into the collision system
+   * 
+   * @param {String} [i = 0] unique id
    * @param {Number} [x = 0] The starting X coordinate
    * @param {Number} [y = 0] The starting Y coordinate
+   * @param {Boolean} [updating = false] Set to true if the body already exists in the BVH (used internally when updating the body's position)
    * @param {Number} [padding = 0] The amount to pad the bounding volume when testing for potential collisions
    * @returns {Point}
    */
-  createPoint (x = 0, y = 0, padding = 0) {
+  createPoint (id, x = 0, y = 0, updating = false, padding = 0) {
     const body = new Point(x, y, padding)
-
-    this._bvh.insert(body)
-
+    this._insertBody(id, body, updating)
     return body
+  }
+
+  /**
+   * 删除id对的body
+   * 
+   * @param {String} id 
+   * @param {Boolean} updating 
+   */
+  remove(id, updating = fasle) {
+    let body = this._id2Body.get(id);
+    this._bvh.remove(body, updating);
   }
 
   /**
@@ -76,30 +130,6 @@ class Collisions {
    */
   static createResult () {
     return new Result()
-  }
-
-  /**
-   * Inserts bodies into the collision system
-   * @param {...Circle|...Polygon|...Point} bodies
-   */
-  insert (...bodies) {
-    for (const body of bodies) {
-      this._bvh.insert(body, false)
-    }
-
-    return this
-  }
-
-  /**
-   * Removes bodies = require(the collision system
-   * @param {...Circle|...Polygon|...Point} bodies
-   */
-  remove (...bodies) {
-    for (const body of bodies) {
-      this._bvh.remove(body, false)
-    }
-
-    return this
   }
 
   /**
@@ -148,6 +178,7 @@ class Collisions {
   }
 };
 
+const collisions = new Collisions();
 module.exports = {
-  Collisions, BVH, Circle, Polygon, Point, Result, SAT
+  collisions, BVH, Circle, Polygon, Point, Result, SAT
 }
